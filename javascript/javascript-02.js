eba7b6b7b5a465f305d48749424c0f2aed8871d3
JFMAREN <fernanmartinez2006@gmail.com>
2026-05-08 11:21:02 -0600

    tarea calculadora

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -14,20 +14,49 @@ function pintar(e) {
     console.log(e.target.innerText);
     resultado.value += e.target.innerText;
 }
+//ññ
 
-function pintar2(e){
+let prm1;
+let operacion = ""; 
+
+function pintar2(e) {
     console.log(e.target.innerText);
-    if (e.target.innerText == "+")
-        suma()
+    const botonPresionado = e.target.innerText;
+
+    if (botonPresionado === "=") {
+        calcular(); 
+    } else if (botonPresionado === "C") {
+        resultado.value = ""; 
+        prm1 = "";
+        operacion = "";
+    } else {
+        prm1 = resultado.value;
+        operacion = botonPresionado;
+        resultado.value = "";
+    }
 }
 
-let prm1
-function suma(){
-    prm1 = resultado.value
-    resultado.value = ""
-}
+function calcular() {
+    let prm2 = resultado.value;
+    let r = 0;
+    
+    let n1 = parseFloat(prm1);
+    let n2 = parseFloat(prm2);
 
-function igual(){
-    prm2 = resultado.value
-    resultado.value = parseInt(prm1) + parseInt(prm2)
+    switch(operacion) {
+        case "+":
+            r = n1 + n2;
+            break;
+        case "-":
+            r = n1 - n2;
+            break;
+        case "x": 
+            r = n1 * n2;
+            break;
+        case "/":
+            r = n2 !== 0 ? n1 / n2 : "Error";
+            break;
+    }
+    
+    resultado.value = r;
 }

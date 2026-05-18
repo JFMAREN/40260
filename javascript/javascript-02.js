7ce4c9c3b7b960644e965c222dc5cd09eb9288b9
JFMAREN <fernanmartinez2006@gmail.com>
2026-05-18 12:11:46 -0600

    js con y sin modulos

--- javascript/javascript-02.js
+++ javascript/javascript-02.js
@@ -6,7 +6,7 @@ for (const key in botones) {
         if (boton.className != "operadores")
             boton.addEventListener("click", pintar)
         else
-            boton.addEventListener("click", pintar2)
+            boton.addEventListener("click", igual)
     }
 }
 
@@ -14,49 +14,80 @@ function pintar(e) {
     console.log(e.target.innerText);
     resultado.value += e.target.innerText;
 }
+
+function operadores(e){
+    let operador = e.target.innerText; 
+    if (operador == "=")
+       igual(); 
+    else
+        almacena_parametro(operador)
+}
+
+let prm1
+let operacion
+function almacena_parametro(prm){
+    //prm1 = resultado.value
+    parametros.push(resultado.value)
+    resultado.value = ""
+    operacion = prm
+}
+
+function igual(){
+    //prm2 = resultado.value
+    parametros.push(resultado.value)   
+    if(operacion == "+")
+        resultado.value = parseFloat(prm1) + parseFloat(prm2)
+    if(operacion == "-")
+        resultado.value = parseFloat(prm1) - parseFloat(prm2)
+    if(operacion == "x")
+        resultado.value = parseFloat(prm1) * parseFloat(prm2)
+    if(operacion == "/")
+        resultado.value = parseFloat(prm1) / parseFloat(prm2)
+}
+
 //ññ
 
-let prm1;
-let operacion = ""; 
+// let prm1;
+// let operacion = ""; 
 
-function pintar2(e) {
-    console.log(e.target.innerText);
-    const botonPresionado = e.target.innerText;
+// function pintar2(e) {
+//     console.log(e.target.innerText);
+//     const botonPresionado = e.target.innerText;
 
-    if (botonPresionado === "=") {
-        calcular(); 
-    } else if (botonPresionado === "C") {
-        resultado.value = ""; 
-        prm1 = "";
-        operacion = "";
-    } else {
-        prm1 = resultado.value;
-        operacion = botonPresionado;
-        resultado.value = "";
-    }
-}
+//     if (botonPresionado === "=") {
+//         calcular(); 
+//     } else if (botonPresionado === "C") {
+//         resultado.value = ""; 
+//         prm1 = "";
+//         operacion = "";
+//     } else {
+//         prm1 = resultado.value;
+//         operacion = botonPresionado;
+//         resultado.value = "";
+//     }
+// }
 
-function calcular() {
-    let prm2 = resultado.value;
-    let r = 0;
+// function calcular() {
+//     let prm2 = resultado.value;
+//     let r = 0;
     
-    let n1 = parseFloat(prm1);
-    let n2 = parseFloat(prm2);
+//     let n1 = parseFloat(prm1);
+//     let n2 = parseFloat(prm2);
 
-    switch(operacion) {
-        case "+":
-            r = n1 + n2;
-            break;
-        case "-":
-            r = n1 - n2;
-            break;
-        case "x": 
-            r = n1 * n2;
-            break;
-        case "/":
-            r = n2 !== 0 ? n1 / n2 : "Error";
-            break;
-    }
+//     switch(operacion) {
+//         case "+":
+//             r = n1 + n2;
+//             break;
+//         case "-":
+//             r = n1 - n2;
+//             break;
+//         case "x": 
+//             r = n1 * n2;
+//             break;
+//         case "/":
+//             r = n2 !== 0 ? n1 / n2 : "Error";
+//             break;
+//     }
     
-    resultado.value = r;
-}
+//     resultado.value = r;
+// }
